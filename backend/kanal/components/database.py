65d6eb9bfb9a4ed38a3933f04a8fda7933434a90
Oleg <oruchkin@bk.ru>
2022-10-03 17:00:38 +0300

    db swithced

--- backend/kanal/components/database.py
+++ backend/kanal/components/database.py
@@ -3,24 +3,24 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': os.environ.get('DB_NAME', default=None),
-#         'USER': os.environ.get('POSTGRES_USER', default=None),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default=None),
-#         'HOST': os.environ.get('DB_HOST', default=None),
-#         'PORT': os.environ.get('DB_PORT', default=None)
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.environ.get('DB_NAME', default=None),
+        'USER': os.environ.get('POSTGRES_USER', default=None),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default=None),
+        'HOST': os.environ.get('DB_HOST', default=None),
+        'PORT': os.environ.get('DB_PORT', default=None)
+    }
+}
 
 
 #ниже на случай если проблемы с .env
